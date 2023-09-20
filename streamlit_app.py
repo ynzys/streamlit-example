783b20bd57a5d34cd93fa6e2d61fe8ec33d3fce1
ynzys <63483486+ynzys@users.noreply.github.com>
2023-09-20 11:06:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
+    Point = namedtuple('Point', 'x z')
     data = []
 
     points_per_turn = total_points / num_turns
